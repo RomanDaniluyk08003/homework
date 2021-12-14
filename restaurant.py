3a075e1b14a9b39acc26599e1cae4698b16982d0
RomanDaniluyk08003 <96053425+RomanDaniluyk08003@users.noreply.github.com>
2021-12-14 20:22:42 +0200

    Add files via upload

--- restaurant.py
+++ restaurant.py
@@ -23,7 +23,7 @@ food_3_cost = int(food_3_quantity) * food_3_price
 total_1 = food_1_cost+food_2_cost+food_3_cost
 total_2 = food_1_cost+food_2_cost+food_3_cost+food_delivery
 delivery = input("Do you need delivery? Y/N ")
-if delivery == 'Y':
+if delivery == 'y':
     if  (food_1_cost+food_2_cost+food_3_cost)>1000:
         print('You have ordered \n',food_1_quantity,' x ',food_1_name,' = ',food_1_cost,'\n',
                                     food_2_quantity,' x ',food_2_name,' = ',food_2_cost,'\n', 
@@ -34,4 +34,9 @@ if delivery == 'Y':
                                     food_2_quantity,' x ',food_2_name,' = ',food_2_cost,'\n', 
                                     food_3_quantity,' x ',food_3_name,' = ',food_3_cost,'\n',
                         'Delivery-',food_delivery,'\n',
-                         'Total - ',total_2)                            
+                         'Total - ',total_2)     
+else :
+    print('You have ordered \n',food_1_quantity,' x ',food_1_name,' = ',food_1_cost,'\n',
+                                    food_2_quantity,' x ',food_2_name,' = ',food_2_cost,'\n', 
+                                    food_3_quantity,' x ',food_3_name,' = ',food_3_cost,'\n',
+                        'Total  - ',total_1)                                                
