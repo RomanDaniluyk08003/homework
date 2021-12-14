f4181686d604b204233057f1d7352c60f8989af1
RomanDaniluyk08003 <96053425+RomanDaniluyk08003@users.noreply.github.com>
2021-12-14 12:51:17 +0200

    Add files via upload

---
+++ line.py
@@ -0,0 +1,11 @@
+figure=input("what figure to draw? ")
+if figure=="1":
+    print("-----")
+elif figure=="parallel horizontal lines":
+    print("-----\n-----")
+elif figure=="parallel vertical lines":
+    print("|  |\n|  |")     
+elif figure=="square":
+    print("-----\n|   |\n|   |\n-----")
+else:
+    print("Can't draw such figure!")
